8c57eeeae544f4a7c392dd537fa24a90856ccfe7
Sven Gato Redsun <svengato@svengato.com>
2023-02-10 10:29:49 -0700

    Update gene and trait keyword search pages to handle latest graphql-server and web-components enhancements

--- tools/js/graphql.js
+++ tools/js/graphql.js
@@ -5,17 +5,33 @@ const uri = 'https://dev.peanutbase.org/graphql';
 // A function that gets data from a GraphQL server via a POST request.
 // Adapted from https://graphql.org/graphql-js/graphql-clients/
 function graphqlQuery(uri, query, variables={}, abortSignal=undefined) {
-  return fetch(uri, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-    signal: abortSignal,
-  }).then(r => r.json());
+    return fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+        signal: abortSignal,
+    }).then(r => r.json());
 }
 // TODO: implement error handling
+
+// Flatten GraphQL results that contain objects
+const flatten = (obj, out={}, prefixes=[]) => {
+    if (obj != null) {
+        Object.keys(obj).forEach(key => {
+            const key_prefixes = [...prefixes, key];
+            if (typeof obj[key] == 'object' && !Array.isArray(obj[key])) {
+                out = flatten(obj[key], out, key_prefixes);
+            } else {
+                const prefix_key = key_prefixes.join('_');
+                out[prefix_key] = obj[key];
+            }
+        });
+    }
+    return out;
+};
